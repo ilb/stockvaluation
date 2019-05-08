f5580e04c4898e48d90ec14c8a97b25a326d4c80
kuznetsov_me <kuznetsov_me@ilb.ru>
2019-05-08 10:06:56 +0000

    output data representation: argument xml/json added

--- fairpricecalc/__main__.py
+++ fairpricecalc/__main__.py
@@ -1,8 +1,10 @@
 from fair_price_calculator import FairPriceCalculator
+from json.decoder import JSONDecodeError
+
 import sys
 import json
-from json.decoder import JSONDecodeError
 import dicttoxml
+import argparse
 
 
 def main():
@@ -13,17 +15,28 @@ def main():
     Then write result ('active', 'price') in json format to the standart 
     output. 
     '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--response-representation', dest='resp_repr', 
+                        action='store', default='application/json')
+
+    args = parser.parse_args()
+    resp_repr = args.resp_repr
+    
     try:
         data = json.load(sys.stdin)
         ticker, date = data['ticker'], data['date']
     except JSONDecodeError:
         raise NameError('JSONDecodeError: wrong input json data, can not convert')
 
-    calculator = FairPriceCalculator(ticker)
-    
+    calculator = FairPriceCalculator(ticker) 
     result = calculator.calculate(date)
-    xml = dicttoxml.dicttoxml(result, attr_type=False, item_func=lambda x: x[:-1])
-    sys.stdout.buffer.write(xml)
+
+    if resp_repr == 'application/json':
+        sys.stdout.write(json.dumps(result)) 
+    elif resp_repr == 'application/xml':
+        xml = dicttoxml.dicttoxml(result, attr_type=False, item_func=lambda x: x[:-1])
+        sys.stdout.buffer.write(xml)
     
 if __name__ == '__main__':
     main()

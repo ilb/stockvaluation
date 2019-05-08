26bcaba331e8026d55ccb141f6cd6d496432e431
SPoket <lol8funny@gmail.com>
2019-05-08 14:06:56 +0400

    output data representation: argument xml/json added

--- run-calc.sh
+++ run-calc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
- cat examples/input.json | env ru.ilb.stockvaluation.securitiesrefurl=https://raw.githubusercontent.com/SPoket/hackathon/master/quests/stockvaluation/stockvaluation/globalvolume/issuancevolume.xhtml python fairpricecalc >> fairpriceorder/data.xml
+ cat examples/input.json | env ru.ilb.stockvaluation.securitiesrefurl=https://raw.githubusercontent.com/SPoket/hackathon/master/quests/stockvaluation/stockvaluation/globalvolume/issuancevolume.xhtml python fairpricecalc --response-representation application/xml >> fairpriceorder/data.xml

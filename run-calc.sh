cfc964a180fe1f029491f633209766d1063e8d55
slavb <slavb@ilb.ru>
2019-05-23 06:54:47 +0000

    docs/issuancevolume.xhtml -> docs/securities.xhtml

--- run-calc.sh
+++ run-calc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
- cat examples/input.json | env ru.bystrobank.apps.stockvaluation.securitiesrefurl=https://raw.githubusercontent.com/bystrobank/stockvaluation/master/fairpricecalc/test/volume.xhtml python fairpricecalc --response-representation application/xml > fairpriceorder/data.xml
+ cat examples/input.json | env ru.bystrobank.apps.stockvaluation.securitiesrefurl=https://bystrobank.github.io/stockvaluation/securities.xhtml python fairpricecalc --response-representation application/xml > fairpriceorder/data.xml

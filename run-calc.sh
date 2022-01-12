52a852e14f3b509b7488661dd703dc7c18c6ec8c
Vjacheslav Borisov <slavb@ilb.ru>
2022-01-12 12:17:49 +0400

    switch to ilb

--- run-calc.sh
+++ run-calc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
- cat examples/input.json | env ru.bystrobank.apps.stockvaluation.securitiesrefurl=https://bystrobank.github.io/stockvaluation/securities.xhtml python fairpricecalc --response-representation application/xml > fairpriceorder/data.xml
+ cat examples/input.json | env ru.bystrobank.apps.stockvaluation.securitiesrefurl=https://ilb.github.io/stockvaluation/securities.xhtml python fairpricecalc --response-representation application/xml > fairpriceorder/data.xml

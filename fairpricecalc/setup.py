eb3ca58a634c4e46eeead896bf0c22ccb008be47
MaximumQuiet <contact@maximumquiet.cyou>
2023-02-27 10:23:06 +0400

    Add dependencies

--- fairpricecalc/setup.py
+++ fairpricecalc/setup.py
@@ -1,10 +1,20 @@
 import os
 from setuptools import setup, find_packages
 
-import app
+app_dependencies = [
+    "pandas==1.5.3",
+    "lxml==4.9.2",
+    "dicttoxml==1.7.16",
+]
 
 setup(
     name='fairpricecalc',
+    version="1.0.0",
+    url="https://github.com/ilb.ru/stockvaluation",
+    description="Сервис расчёта справедливой цены ЦБ",
+    author="Kuznetsov Maxim",
+    extras_require={"dev": {} },
+    install_requires=app_dependencies,
     entry_points={
         'console_scripts': [
             'stockvaluation = __main__:main',]}
